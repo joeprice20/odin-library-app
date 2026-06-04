85093403caa4d515b29bc08105f9ab2477ee8cd9
Joe <19871383+joeprice20@users.noreply.github.com>
2026-06-04 12:48:52 +0100

    add id property to Book class

--- src/script.ts
+++ src/script.ts
@@ -3,12 +3,15 @@ class Book {
     author: string;
     pages: number;
     read: boolean;
+    id: string;
 
     constructor(title: string, author: string, pages: number, read: boolean) {
         this.title = title;
         this.author = author;
         this.pages = pages;
         this.read = read;
+
+        this.id = crypto.randomUUID()
     }
 
     info(){
